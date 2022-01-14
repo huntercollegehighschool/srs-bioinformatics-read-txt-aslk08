c66cce0c9c285bc06169f79f30cb89b55c8e1552
aslk <79598722+aslk08@users.noreply.github.com>
2022-01-14 17:21:59 +0000

    completed

--- Baminoacids.py
+++ Baminoacids.py
@@ -5,4 +5,11 @@ weightedstring should read from masstable.txt. It's helpful to have those masses
 """
 
 def weightedstring(protein):
-  pass
+  weights = {}
+  total = 0
+  file = open("masstable.txt", "r")
+  for weight in file.readlines():
+    weights[weight[0]] = float(weight[2:])
+  for p in protein:
+    total += weights[p]
+  return total
